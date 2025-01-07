fb1d68958215c37719e9ac6ea4274aa2464d4068
Anna <32278291+annasudol@users.noreply.github.com>
2025-01-07 16:24:39 +0100

    Remove Tenderly configuration and related environment variables from the project

--- src/config/web3.ts
+++ src/config/web3.ts
@@ -9,17 +9,11 @@ import { bscTestnet, sepolia } from 'viem/chains';
 import { envs } from '@/lib/envs';
 
 import { AppConfig } from './AppConfig';
-import { sepoliaTenderly } from './tenderly-config';
 
 export const rainbowConfig = getDefaultConfig({
   appName: AppConfig.title,
   projectId: envs.NEXT_PUBLIC_RAINBOWKIT_PROJECT_ID,
-  chains: [
-    sepolia,
-    bscTestnet,
-    ...(envs.NEXT_PUBLIC_TENDERLY_VNETS_ENABLED ? [sepoliaTenderly] : []),
-    ...(envs.NEXT_PUBLIC_ENABLE_TESTNETS ? [sepolia] : []),
-  ],
+  chains: [sepolia, bscTestnet],
   wallets: [
     {
       groupName: 'Popular',
@@ -29,6 +23,4 @@ export const rainbowConfig = getDefaultConfig({
   ssr: false,
 });
 
-export const initialChain = envs.NEXT_PUBLIC_TENDERLY_VNETS_ENABLED
-  ? sepoliaTenderly
-  : rainbowConfig.chains[0];
+export const initialChain = rainbowConfig.chains[0];
--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -1,20 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createEnv } from '@t3-oss/env-nextjs';
 
-import { ADDRESS, ONLY_BOOLEAN, STRING_NOT_EMPTY } from '@/lib/zod';
+import { ADDRESS, STRING_NOT_EMPTY } from '@/lib/zod';
 
 export const envs = createEnv({
   client: {
     // RainbowKit
     NEXT_PUBLIC_RAINBOWKIT_PROJECT_ID: STRING_NOT_EMPTY,
-
-    // Tenderly
-    NEXT_PUBLIC_TENDERLY_PROJECT_ID: STRING_NOT_EMPTY,
-    NEXT_PUBLIC_TENDERLY_VNETS_ENABLED: ONLY_BOOLEAN,
-    NEXT_PUBLIC_ENABLE_TESTNETS: ONLY_BOOLEAN,
-
-    // Etherscan
-    NEXT_PUBLIC_ETHERSCAN_API_KEY: STRING_NOT_EMPTY,
 
     // Contract Addresses
     NEXT_PUBLIC_BRIDGE_SEPOLIA_ADDRESS: ADDRESS,
@@ -25,15 +17,7 @@ export const envs = createEnv({
     NEXT_PUBLIC_RAINBOWKIT_PROJECT_ID:
       process.env.NEXT_PUBLIC_RAINBOWKIT_PROJECT_ID,
 
-    // Tenderly
-    NEXT_PUBLIC_TENDERLY_PROJECT_ID:
-      process.env.NEXT_PUBLIC_TENDERLY_PROJECT_ID,
-    NEXT_PUBLIC_TENDERLY_VNETS_ENABLED:
-      process.env.NEXT_PUBLIC_TENDERLY_VNETS_ENABLED,
-    NEXT_PUBLIC_ENABLE_TESTNETS: process.env.NEXT_PUBLIC_ENABLE_TESTNETS,
-
     // Etherscan
-    NEXT_PUBLIC_ETHERSCAN_API_KEY: process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY,
     NEXT_PUBLIC_BRIDGE_SEPOLIA_ADDRESS:
       process.env.NEXT_PUBLIC_BRIDGE_SEPOLIA_ADDRESS,
     NEXT_PUBLIC_BRIDGE_BSCSCAN_ADDRESS:
